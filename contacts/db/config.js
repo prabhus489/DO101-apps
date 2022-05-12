6a7b9df04f9e86091f6030c15e1fc2a44ec33dfb
Prabhu Sundarraj <prabhu12yuva@gmail.com>
2022-05-12 21:18:45 +0530

    updating DBCONFIG variable name to match openshift regex

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
+const DBCONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
-    connectionString: DB_CONFIG,
+    connectionString: DBCONFIG,
     ssl: false,
 });
   
